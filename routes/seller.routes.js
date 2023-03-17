db68883d9febb107785b566f0e44388c2cc6ee6b
impapy <impapy1996@gmail.com>
2023-03-17 14:15:44 +0200

    first commit

---
+++ routes/seller.routes.js
@@ -0,0 +1,28 @@
+const router = require("express").Router();
+const seller = require("../controller/seller.controller");
+const verfiy = require("../controller/verifyTokenapi.controller.js");
+const products = require("../controller/product.controller.js")
+const express = require('express');
+const path = require('path');
+const {upload}= require('../helpers/filehelper');
+
+
+
+router.post("/addproduct", express.static(path.join(__dirname, 'uploads')), upload.array('files'),verfiy.verifyTokenAndAuthorization, products.createProduct);
+router.get("/", verfiy.verifyTokenAndAdmin, seller.getAdminOrders);
+router.get("/sellerOrders/:id", verfiy.verifyTokenAndAdmin, seller.getsellerOrders);
+router.get("/getSellers", verfiy.verifyTokenAndAdmin, seller.getAllSellers);
+router.post("/model",  products.AddmodelElec);
+router.put("/up/mod/:id",products.ubdateModel);
+
+router.get("/sellerProd", verfiy.verifyTokenAndAuthorization, seller.getProductSeller);
+router.put("/delProd/:id", verfiy.verifyTokenAndAuthorization, seller.deleteProductSeler);
+router.get("/getProd/:id", verfiy.verifyTokenAndAuthorization, seller.getProductdetails);
+router.put("/updatePro/:id", verfiy.verifyTokenAndAuthorization, express.static(path.join(__dirname, 'uploads')), upload.array('files'), seller.updateProduct);
+router.get("/getRev/:id", verfiy.verifyTokenAndAuthorization, seller.getProductReviews);
+router.put("/:productId/:reviewId", seller.deleteReview);  
+router.get("/getSingleOrder/:id", verfiy.verifyTokenAndAuthorization, seller.getAdminsingleOrders);
+
+
+
+module.exports = router;
